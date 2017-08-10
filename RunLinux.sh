d687ec8d91a843a4fb26ec962ccd35ac4f768013
anjan <anjanmondal@gmail.com>
2017-08-10 19:10:36 +0530

    ghfgj

---
+++ RunLinux.sh
@@ -0,0 +1,2 @@
+sh  /home/rahul/soap/SoapUI-5.3.0/bin/testrunner.sh -s"Time Zone API TestSuite" -c"Simple Tests" -r -a -j -J -f/home/rahul/SoapPi/workspace/soaptest/Result /home/rahul/SoapPi/workspace/soaptest/Google-Maps-soapui-project.xml
+
