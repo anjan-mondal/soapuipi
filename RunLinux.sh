2e963efd870e12499e106f907e7df3fc946714cd
anjan <anjanmondal@gmail.com>
2017-08-14 22:41:18 +0530

    tes

--- RunLinux.sh
+++ RunLinux.sh
@@ -1,2 +1,2 @@
-sh  /home/rahul/soap/SoapUI-5.3.0/bin/testrunner.sh -s"Time Zone API TestSuite"  -r -a -j -J -f/media/rahul/U/SeleniumPi/workspace/soaptest/Result /media/rahul/U/SeleniumPi/workspace/soaptest/Projects/googleAPI.xml
+sh  /home/rahul/soap/SoapUI-5.3.0/bin/testrunner.sh -s"Time Zone API TestSuite"  -r -a -f/media/rahul/U/SeleniumPi/workspace/soaptest/Result /media/rahul/U/SeleniumPi/workspace/soaptest/Projects/googleAPI
 
