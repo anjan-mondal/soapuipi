654fff8301d0e0143302c4a9c6f058669a403aae
anjan <anjanmondal@gmail.com>
2017-08-14 20:19:40 +0530

    rtt

--- RunLinux.sh
+++ RunLinux.sh
@@ -1,2 +1,2 @@
-sh  /home/rahul/soap/SoapUI-5.3.0/bin/testrunner.sh -s"Time Zone API TestSuite" -c"Simple Tests" -r -a -j -J -f/home/rahul/SeleniumPi/workspace/soaptest/Result /home/rahul/SeleniumPi/workspace/soaptest/Projects/Google-Maps-soapui-project.xml
+sh  /home/rahul/soap/SoapUI-5.3.0/bin/testrunner.sh -s"Time Zone API TestSuite" -c"Simple Tests" -r -a -j -J -f/media/rahul/U/SeleniumPi/workspace/soaptest/Result /media/rahul/U/SeleniumPi/workspace/soaptest/Projects/Google-Maps-soapui-project.xml
 
